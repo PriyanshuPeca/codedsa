b695d882bdbf625b594eb5f584ed3c1d4b24069c
PriyanshuPeca <priyanshu22223@gmail.com>
2025-06-06 09:30:44 +0530

    Added new files

---
+++ TESTO.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+
+const int MOD=1e9+7;
+const int INF=LLONG_MAX>>1;
+void solve(){
+cout<<1<<endl;
+}
+signed main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0),cout.tie(0);
+    int t;
+    //t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
